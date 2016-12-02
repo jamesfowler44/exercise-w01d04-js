3262bd421c585edd8930d38287a70227abb095c4
james fowler <jamesfowler1000@gmail.com>
2016-12-01 21:58:14 -0800

    finishing part 8

--- main.js
+++ main.js
@@ -2,23 +2,43 @@
 // Write a function called knighted that takes a name and returns it with "Sir" in front
 // when you use it - it should look like this
 
-// var name = knighted('Elton');
-// console.log(name) => 'Sir Elton'
+function knighted(yourName) {
+  return "Sir " + yourName;
+}
+
+var name = knighted('Elton');
+// console.log(name);
 
 // task 2
 // Write a function called doubleIt that takes a number, doubles it and returns the new number
 
-// var answer = doubleIt(2);
-// console.log(answer) => 4
+var doubleIt = function(n) {
+   return n * 2;
+}
+
+var answer = doubleIt(2);
+
+console.log(answer);
 
 // task 3
 // Write a function called makeUser that takes a name and returns a new object
 
-// var user = makeUser('Dave');
-// console.log(user) => {name: 'Dave'}
+var makeUser = function(userName) {
+   var userObject = {name: userName};
+   return userObject;
+}
+
+var user = makeUser('Dave');
+console.log(user);
 
 // task 4
 // Write a function called makeLink that takes a title and a url and returns a string of html
+var makeLink = function(title, url) {
+  return '<a href=' + url + '>' + title + '</a>';
+};
+var aTag = makeLink('google', 'http://google.com');
+console.log(aTag);
+
 
 // var aTag = makeLink('google', 'http://www.google.com');
 // console.log(aTag) => '<a href="http://www.google.com">google</a>'
@@ -28,30 +48,80 @@
 
 // var answer = second(['a', 'b', 'c']);
 // console.log(answer) => 'b'
-
+var second = function(array) {
+    return array[1];
+};
+var answer = second(['a', 'b', 'c']);
+console.log(answer);
 // task 6
 // Write a function called toSentence that takes an array and returns a string
 // where the items are joined by commas (except the last one which is joined with 'and')
 // where the first letter is capitalized and the string ends in an exclamation point.
-
-// var sentence = toSentence(['lions', 'tigers', 'bears']);
-// console.log(sentence) // 'lions, tigers, and bears!'
+var toSentence = function(array) {
+    var string = array[0] + ', ' + array[1] + ', ' + 'and ' + array[2] + '!';
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+var sentence = toSentence(['lions', 'tigers', 'bears']);
+console.log(sentence);
 
 // task 7
 // Write a function called random number that returns a random number between 0 and x
 // Hint: Take a look at MDN's docs on Math.random
-// var answer = randomNumber(5);
-// console.log(answer) // 0..5
+var randomNumber = function(max) {
+  min = Math.floor(0);
+  max = Math.ceil(5);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+var answer = randomNumber(0, 5);
+console.log(answer);
 
 // task 8
 // Write a function called magicEightball that returns a random answer from the following list
 // "yes", "no", "maybe", "ask again later", "most likely"
-// var answer = magicEightball();
-// console.log(answer) // yes || no || maybe etc...
-
+var magicEightball = function() {
+  array = ["yes", "no", "maybe", "ask again later", "most likely"];
+  min = Math.floor(0);
+  max = Math.ceil(5);
+  var rand = array[Math.floor(Math.random() * array.length)];
+  return rand;
+};
+var answer = magicEightball();
+console.log(answer);
 // task 9
 // write a function called pluck that takes an array of objects and a key as arguments
 // and returns a list of those properties
 // var students = [{name: 'Harry', age: 12}, {name: 'Hermione', age: 13}, {name: 'Draco', age: 13}];
 // var names = pluck(students, 'name');
 // console.log(names) // ['Harry', 'Hermione', 'Draco']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
